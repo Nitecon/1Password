cab1cc87205af254c199457bc956dbd55cfcb30d
Will Hattingh <w.hattingh@nitecon.com>
2018-02-19 12:22:59 -0500

    Ignoring idea and starting config search

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,27 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"os/user"
+)
+
+type ConfigData struct {
+	VaultLocations []string
+}
 
 func main() {
 	fmt.Println("Starting up...")
+	cu, err := user.Current()
+	if err != nil {
+		log.Printf("Must be a valid user account in order to use this tool: %s\n", err)
+		os.Exit(1)
+	}
+	confPath := fmt.Sprintf("%s%s%s", cu.HomeDir, string(os.PathSeparator), ".1Password-config.json")
+	if _, err := os.Stat(confPath); os.IsNotExist(err) {
+		log.Println("No config file found creating initial setup...")
+		ioutil.WriteFile
+	}
 }
